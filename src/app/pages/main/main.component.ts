273bee30799fe40e59bf12073b4645ba65ac6c8b
renamoo <renamoo808@gmail.com>
2020-07-20 22:42:08 +0900

    #29 change top layout

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,11 +7,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  menus = [
+    {
+      id: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/Rena_moo'
+    },
+    {
+      id: 'github',
+      icon: 'github',
+      link: 'https://github.com/renamoo'
+    },
+    {
+      id: 'blog-tech',
+      icon: 'pencil',
+      link: 'https://qiita.com/rena_m'
+    },
+    {
+      id: 'blog-misc',
+      icon: 'pencil',
+      link: 'https://renamoo.hatenablog.com/'
+    },
+    {
+      id: 'profile',
+      icon: 'notification',
+      routerLink: 'profile'
+    },
+    {
+      id: 'works',
+      icon: 'briefcase',
+      routerLink: 'works'
+    }
+  ];
 
   constructor(private router: Router) { }
 
-  onClick(toGo: string) {
-    this.router.navigate([`/${toGo}`]);
+  onClick(id: string) {
+    const menu = this.menus.find(m => m.id === id);
+    if (menu.link) {
+      window.open(menu.link, '_blank');
+    } else if (menu.routerLink) {
+      this.router.navigate([`/${menu.routerLink}`]);
+    }
   }
 
 }
